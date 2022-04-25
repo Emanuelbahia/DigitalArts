39640e68d2824b215cdaa6e125302bd2fedb00f9
francofeierstein <feiersteinfranco@gmail.com>
2022-04-25 19:09:07 -0300

    app.js cambiado

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-let express = require("express");
-let app = express()
 let path = require("path")
-=======
 const express = require("express");
 const app = express();
 
@@ -13,4 +9,3 @@ app.listen(3000, () => console.log("hola mundo"));
 app.get("/", function (req, res) {
   res.send("bienvenido a contacto");
 });
->>>>>>> eba1968677f902c594e372f60b22db3b4eb9a416
