eba1968677f902c594e372f60b22db3b4eb9a416
Emanuelbahia <emanuelazcona@hotmail.com>
2022-04-25 18:38:48 -0300

    cuadros deco a wireframe

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let express = require("express");
-console.log(express);
+const express = require("express");
+const app = express();
+
+console.log(app);
+
+app.listen(3000, () => console.log("hola mundo"));
+
+app.get("/", function (req, res) {
+  res.send("bienvenido a contacto");
+});
