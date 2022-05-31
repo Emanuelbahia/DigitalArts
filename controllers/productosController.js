d09487441b3af9d452d1d1447d9ac2913bef53b4
CarolinaCirillo <carolinacirillo@hotmail.com>
2022-05-30 21:41:23 -0300

    arreglando productocontrollers

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,26 +1,12 @@
-const cuadrosArtistasNuevos = [
-  { id: 1,
-    imagen: "images/artista nuevo 1.jpg.jpg",
-    tecnica: "abstracto con espátula",
-    artista: "Stephen King",
-    dimensiones: "30x40",
-    detalle: "Bastidor en lienzo"
-  }
-
-
-
-
-]
 
 const productosController= { 
   cuadros_decorativos: function (req, res) {
       res.render("cuadros_decorativos");
    },
   cuadros_artistas_nuevos: function (req, res) {
-    let plato = this.cuadrosArtistasNuevos.find(( cuadro ) => cuadro.id == req.params.cuadroId) ;
-    console.log(cuadro);
-    res.render('cuadro_artistas_nuevos', {cuadro : cuadro});
-  },
+    res.render("cuadros_artistas_nuevos");
+ },
+  
   abstracto_espatula: function (req, res) {
     res.render("abstracto_espatula");
  },
