632da2b47aa2349c381308cd940050680383730e
Emanuelbahia <emanuelazcona@hotmail.com>
2022-06-09 19:08:52 -0300

    nuevas vistas y formularios

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,25 +1,36 @@
+const path = require("path");
+const fs = require("fs");
 
-const productosController= { 
-  cuadros_decorativos: function (req, res) {
-      res.render("cuadros_decorativos");
-   },
+const cuadrosFilePath = path.join(__dirname, "../data/products.json");
+const cuadros = JSON.parse(fs.readFileSync(cuadrosFilePath, "utf-8"));
+
+const productosController = {
+  cuadros: function (req, res) {
+    let cuadros = JSON.parse(fs.readFileSync(cuadrosFilePath, "utf-8"));
+    let category = req.params.category;
+    let categoryProducts = cuadros.filter((cuadro) => {
+      return cuadro.category == "AbstractoEspátula";
+    });
+    res.render("products", { categoryProducts });
+  },
+
+  /*
   cuadros_artistas_nuevos: function (req, res) {
     res.render("cuadros_artistas_nuevos");
- },
-  
+  },
+
   abstracto_espatula: function (req, res) {
     res.render("abstracto_espatula");
- },
+  },
   abstracto_pincel: function (req, res) {
     res.render("abstracto_pincel");
- },
+  },
   fotomontaje: function (req, res) {
     res.render("fotomontaje");
- },
+  },
   pouring: function (req, res) {
     res.render("pouring");
- },
-
-}
+  },*/
+};
 
 module.exports = productosController;
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const productosController = require("../controllers/productosController");
 const router = express.Router();
-
+/*
 router.get("/cuadros_decorativos", productosController.cuadros_decorativos);
-router.get("/cuadros_artistas_nuevos", productosController.cuadros_artistas_nuevos);
+router.get("/cuadros_artistas_nuevos",productosController.cuadros_artistas_nuevos);
 router.get("/abstracto_espatula", productosController.abstracto_espatula);
 router.get("/abstracto_pincel", productosController.abstracto_pincel);
 router.get("/fotomontaje", productosController.fotomontaje);
 router.get("/pouring", productosController.pouring);
+*/
 
+router.get("/products", productosController.cuadros);
 
 module.exports = router;
