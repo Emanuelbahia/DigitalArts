406122c2ffc3f97a540c7e93945762cae3a99aa5
Emanuelbahia <emanuelazcona@hotmail.com>
2022-06-10 18:54:17 -0300

    rutas edit, detail, create, delete y vista detalle del producto

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -5,14 +5,38 @@ const cuadrosFilePath = path.join(__dirname, "../data/products.json");
 const cuadros = JSON.parse(fs.readFileSync(cuadrosFilePath, "utf-8"));
 
 const productosController = {
+  /* categoria de cuadros */
   cuadros: function (req, res) {
-    let cuadros = JSON.parse(fs.readFileSync(cuadrosFilePath, "utf-8"));
+    const cuadros = JSON.parse(fs.readFileSync(cuadrosFilePath, "utf-8"));
     let category = req.params.category;
     let categoryProducts = cuadros.filter((cuadro) => {
       return cuadro.category == category;
     });
     res.render("products", { categoryProducts });
   },
+
+  /* detalle del producto */
+  detail: function (req, res) {
+    const cuadros = JSON.parse(fs.readFileSync(cuadrosFilePath, "utf-8"));
+    let id = req.params.id;
+
+    let detailProduct = cuadros.find((cuadro) => cuadro.id == id);
+
+    return res.render("detail", { detailProduct });
+  },
+
+  /* crear producto */
+  formCreate: function (req, res) {},
+
+  create: function (req, res) {},
+
+  /* editar producto */
+  formEdit: function (req, res) {},
+
+  edit: function (req, res) {},
+
+  /* eliminar producto */
+  delete: function (req, res) {},
 };
 
 module.exports = productosController;
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const path = require("path");
 const productosController = require("../controllers/productosController");
 const router = express.Router();
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/images/products/"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + " - " + path.extname(file.originalname));
+  },
+});
+
+/* ruta categoria de cuadros */
 router.get("/products/:category", productosController.cuadros);
+
+/* ruta detalle de producto */
+router.get("/products/detail/:id", productosController.detail);
+
+/* ruta de crear un producto */
+router.get("/products/create", productosController.formCreate);
+router.post("/products", productosController.create);
+
+/* ruta de editar un producto */
+router.get("products/edit/:id", productosController.formEdit);
+router.put("/products/edit/:id", productosController.edit);
+
+/* ruta de eliminar un producto */
+router.delete("/products/detele/:id", productosController.delete);
 
 module.exports = router;
