6793736b64e0c02b0e3dc7b8125fa0416eb94ff4
Emanuelbahia <emanuelazcona@hotmail.com>
2022-06-10 15:15:43 -0300

    estilo products y nuevo banner

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -13,24 +13,6 @@ const productosController = {
     });
     res.render("products", { categoryProducts });
   },
-
-  /*
-  cuadros_artistas_nuevos: function (req, res) {
-    res.render("cuadros_artistas_nuevos");
-  },
-
-  abstracto_espatula: function (req, res) {
-    res.render("abstracto_espatula");
-  },
-  abstracto_pincel: function (req, res) {
-    res.render("abstracto_pincel");
-  },
-  fotomontaje: function (req, res) {
-    res.render("fotomontaje");
-  },
-  pouring: function (req, res) {
-    res.render("pouring");
-  },*/
 };
 
 module.exports = productosController;
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const productosController = require("../controllers/productosController");
 const router = express.Router();
-/*
-router.get("/cuadros_decorativos", productosController.cuadros_decorativos);
-router.get("/cuadros_artistas_nuevos",productosController.cuadros_artistas_nuevos);
-router.get("/abstracto_espatula", productosController.abstracto_espatula);
-router.get("/abstracto_pincel", productosController.abstracto_pincel);
-router.get("/fotomontaje", productosController.fotomontaje);
-router.get("/pouring", productosController.pouring);
-*/
 
 router.get("/products/:category", productosController.cuadros);
 
