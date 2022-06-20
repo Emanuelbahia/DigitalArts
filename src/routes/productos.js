e0e2dac31e82791a3342daaccb41b41094f1c802
Emanuelbahia <emanuelazcona@hotmail.com>
2022-06-20 18:52:00 -0300

    new images

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../../public/images/news"));
+    cb(null, path.join(__dirname, "../../public/images"));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
