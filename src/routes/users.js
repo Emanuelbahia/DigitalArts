f307faf721b7e8ae4157abdaefb762c0a0735d6d
CarolinaCirillo <carolinacirillo@hotmail.com>
2022-06-21 22:53:25 -0300

    comenzando validator register

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const usersController = require("../controllers/usersController");
 const router = express.Router();
+const multer = require ("multer");
+const path = require ("path");
+const { body } = require("express-validator")
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, "./public/images"));
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
+  const upload = multer({ storage });
+
+  const validations = [
+    body('name').notEmpty,
+    body('surname').notEmpty,
+    body('email').notEmpty,
+    body('password').notEmpty,
+  ]
+
+// formulario de registro
 router.get("/register", usersController.register);
+
+// procesar el registro
+router.post("/register", upload.single('avatar'),validations, usersController.processRegister);
+
+
 router.get("/registro_de_artistas", usersController.registro_de_artistas);
+
+
 
 module.exports = router;
