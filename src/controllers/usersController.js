6d1f37334510e55a945e25e59c17276f0266a0eb
Emanuelbahia <emanuelazcona@hotmail.com>
2022-06-23 16:41:54 -0300

    validaciones de formulario y creacion de usuario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,22 +1,42 @@
-const { validationResult }= require('express-validator');
+const path = require("path");
+const fs = require("fs");
 
-const usersController = {  
-    register: function (req, res) {
-        res.render("register");
+const usersFilePath = path.join(__dirname, "../data/users.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+const { validationResult } = require("express-validator");
+
+const usersController = {
+  register: function (req, res) {
+    return res.render("register");
   },
-    processRegister: function (req, res)  {
-     const resultValidation =  validationResult(req);
+  processRegister: function (req, res) {
+    const resultValidation = validationResult(req);
 
-     if (resultValidation.errors.length > 0) {
-      return res.render ('register', { 
+    if (resultValidation.errors.length > 0) {
+      return res.render("register", {
         errors: resultValidation.mapped(),
+        oldData: req.body,
       });
-     }  
-   
-     },
-    registro_de_artistas: function (req, res) {
-        res.render("registro_de_artistas");
-  }
+    }
+
+    const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+    let newUser = {
+      id: users[users.length - 1].id + 1,
+      firstName: req.body.name,
+      lastName: req.body.surname,
+      email: req.body.email,
+      password: req.body.password,
+      image: req.file.filename,
+    };
+    users.push(newUser);
+
+    let newUserSave = JSON.stringify(users, null, 2);
+    fs.writeFileSync(usersFilePath, newUserSave, "utf-8");
+
+    return res.send("no tienes errores");
+  },
 };
 
 module.exports = usersController;
